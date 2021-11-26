f7b0a03e85732a6a4ef630eb14d5552c2ac88e52
Eduardo Mendez <eddxw@Eduardos-MacBook-Pro.local>
2021-11-25 18:35:20 -0700

    refactor enchiper and add more test

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,30 +1,34 @@
-class CaesarCipher
+# The action of a Caesar cipher is to replace each plaintext letter with a different one a fixed number of places down the alphabet.
+module CaesarCipher
   # a(97) — z (122)
   # A(65) — Z (90)
-  def encrypt(message, shift = 5)
-    real_shift = shift % 26
-    encrypted_message = ""
-    message.each_char do |c|
-      ord = c.ord
-      shifted_ord = ord + real_shift
-      if ord >= 65 && ord <= 90
-        if shifted_ord > 90
-          shifted_ord -= 26
-        elsif shifted_ord < 65
-          shifted_ord += 26
-        end
-        encrypted_message << shifted_ord.chr
-      elsif ord >= 97 && ord <= 122
-        if shifted_ord > 122
-          shifted_ord -= 26
-        elsif shifted_ord < 97
-          shifted_ord += 26
-        end
-        encrypted_message << shifted_ord.chr
-      else
-        encrypted_message << c
+  class << self
+    # Encrypt a message using the Caesar Cipher algorithm with a specified shift
+    def encipher(message, shift = 5)
+      encrypted_message = ""
+      message.each_char do |char|
+        encrypted_ord = encipher_ordinal(char.ord, shift)
+        encrypted_message << encrypted_ord
       end
+      encrypted_message
     end
-    encrypted_message
+
+    # encrypt_ordinal(ord, shift) = (ord + shift) % number_of_letters_in_the_alphabet
+    #
+    # If the alphabet base is distinct of 0, as is the case in UTF-8.
+    # Calculate the alphabet base of an ordinal number, either lowercase (65) or uppercase (97)
+    #
+    # encrypt_ordinal(ord, shift) = (ord+shift-base) % 26 + alphabet_base
+    def encipher_ordinal(number, shift)
+      alphabet_base = number <= 90 ? 65 : 97 # If number is <= 90 then its base is uppercase else is lowercase
+
+      alphabet_letter?(number) ? (number + shift - alphabet_base) % 26 + alphabet_base : number
+    end
+
+    # is the ordinal number an alphabet letter?
+    # a — z (97- 122) or A — Z (65 - 90)
+    def alphabet_letter?(ordinal_number)
+      ordinal_number.between?(97, 122) || ordinal_number.between?(65, 90)
+    end
   end
 end
--- caesar_cipher/spec/caesar_spec.rb
+++ caesar_cipher/spec/caesar_spec.rb
@@ -2,19 +2,31 @@ require "spec_helper"
 require_relative "../lib/caesar_cipher"
 
 RSpec.describe "CaesarCipher" do
-  subject(:message) { CaesarCipher.new }
+  subject(:cipher) { CaesarCipher }
 
-  describe "#encrypt" do
+  describe "#encipher" do
     it "works with the provided example and a shift of 5" do
-      expect(message.encrypt("What a string!", 5)).to eql("Bmfy f xywnsl!")
+      expect(cipher.encipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
     end
 
     it "works with the provided example and a shift of 57" do
-      expect(message.encrypt("What a string!", 57)).to eql("Bmfy f xywnsl!")
+      expect(cipher.encipher("What a string!", 57)).to eql("Bmfy f xywnsl!")
     end
 
     it "works with the provided example and a shift of -21" do
-      expect(message.encrypt("What a string!", -21)).to eql("Bmfy f xywnsl!")
+      expect(cipher.encipher("What a string!", -21)).to eql("Bmfy f xywnsl!")
+    end
+
+    it "wotks with wikepedia example with a shift of 23" do
+      expect(cipher.encipher("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23)).to eql("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD")
+    end
+
+    it "wotks with wikepedia example with a shift of -3" do
+      expect(cipher.encipher("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", -3)).to eql("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD")
+    end
+
+    it "if enciphered twice with a shift of 13, gets the original message" do
+      expect(cipher.encipher(cipher.encipher("Hola Mundo!", 13), 13)).to eql("Hola Mundo!")
     end
   end
 end
