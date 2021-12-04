f2ee6dac6988e66ff7467bcd3ae618769cb37753
Eduardo Mendez <joseeduardomendezb@gmail.com>
2021-12-04 16:00:26 -0700

    add yard doc

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,11 +1,9 @@
-# Implement a method #substrings that takes a word as the first
-# argument and then an array of valid substrings (your dictionary)
-# as the second argument. It should return a hash listing each
-# substring (case insensitive) that was found in the original string
-# and how many times it was found.
-
-# This method smells of :reek:NestedIterators
+# It returns a hash listing each substring (case insensitive) that was found in the text and how many times it was found.
+# @note Takes a word as the first argument and then an array of valid substrings as the second argument.
 # @return [Hash]
+# @param text [String] some random text
+# @param dictionary [Array<String>] allowed substrings
+# This method smells of :reek:NestedIterators
 def substrings(text, dictionary)
   sub_strings = Hash.new(0)
   dictionary.each do |allowed_string|
@@ -18,3 +16,5 @@ dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 
 p substrings("below", dictionary)
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+substrings
